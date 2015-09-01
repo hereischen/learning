8ecb8b0513f945265e866baa60bd98c7006df341
Haotong Chen <hereischen@gmail.com>
2015-09-01 11:56:53 +0800

    adding if main

--- python/process.py
+++ python/process.py
@@ -5,9 +5,10 @@ import os
 def run_proc(name):
     print('run child process %s (%s)...' % (name, os.getpid()))
 
-print('Parent process %s.' % os.getpid())
-p = Process(target=run_proc, args=('test',))
-print('child process will start.')
-p.start()
-p.join()
-print('child process ends.')
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('child process will start.')
+    p.start()
+    p.join()
+    print('child process ends.')
