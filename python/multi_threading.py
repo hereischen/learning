32964b4edabbdb5b7c9c0b0c1206278fe5331457
Haotong Chen <hereischen@gmail.com>
2016-03-31 17:09:12 +0800

    adding threading

--- python/multi_threading.py
+++ python/multi_threading.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
+import threading
 from time import ctime, sleep
 
 
-def music():
+def music(name):
     for i in range(2):
-        print "激情听歌 %s" % ctime()
+        print "激情听歌 %s. 听完已经是 %s" % (name, ctime())
         sleep(2)
 
 
-def move():
+def movie(name):
     for i in range(2):
-        print "激情看电影 %s" % ctime()
+        print "激情看电影 %s. 看完已经是 %s" % (name, ctime())
         sleep(5)
 
+threads = []
+t1 = threading.Thread(target=music, args='七子之歌')
+threads.append(t1)
+t2 = threading.Thread(target=movie, args='荒野猎人')
+threads.append(t2)
 if __name__ == '__main__':
-    music()
-    move()
+    for th in threads:
+        th.setDaemon(True)
+        th.start()
+
     print "完成>>>>> %s" % ctime()
