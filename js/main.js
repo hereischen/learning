de3132366342207068a8a2f443c461b4eca833b1
Haotong Chen <hachen@redhat.com>
2017-04-05 16:28:19 +0800

    clean up repos

--- js/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var hello = require('./hello');
-
-var s = 'Michael';
-
-// shit.greet(s);
-hello.shit(s);
---
+++ python/anagram.py
@@ -0,0 +1,5 @@
+def anagram1(s1, s2):
+   alist = list(s2)
+
+   pos1 = 0
+
